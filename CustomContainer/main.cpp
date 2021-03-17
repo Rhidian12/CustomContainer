147dc0dae979f6540e4d5d17faed83063ccfe390
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-03-17 10:20:27 +0100

    Redid the benchmarking

--- CustomContainer/main.cpp
+++ CustomContainer/main.cpp
@@ -2,65 +2,70 @@
 #include <numeric> // std::accumulate
 #include <chrono> // std::chrono
 #include <vector> // std::vector
+#include <fstream> // std::ofstream
+#include <algorithm> // std::max_element
 
 int main(int argc, char* argv[])
 {
 	using Timepoint = std::chrono::high_resolution_clock::time_point;
 
-	std::vector<float> timeOfSTL{};
-	std::vector<float> timeOfCustom{};
+	std::ofstream file{};
+	file.open("BenchmarksReleaseX64.txt");
 
-	const int amountOfIterations{ 500 };
-	const int amountOfPushbacks{ 500 };
+	const int amountOfIterations{ 100 };
+	const int amountOfPushbacks{ 50 };
 
 	std::cout << "Amount of Iterations: " << amountOfIterations << std::endl;
 	std::cout << "Amount of push_back: " << amountOfPushbacks << std::endl;
+	file << "Amount of push_back: " << amountOfPushbacks << std::endl;
 
 	for (int i{}; i < amountOfIterations; ++i)
 	{
-		const Timepoint t1{ std::chrono::high_resolution_clock::now() };
+		{
+			std::vector<long long> times{};
+			for (int j{}; j < amountOfIterations; ++j)
+			{
+				const Timepoint t1{ std::chrono::high_resolution_clock::now() };
 
-		std::vector<int> vector{};
+				std::vector<int> vector{};
 
-		for (int j{}; j < amountOfPushbacks; ++j)
-			vector.push_back(j);
+				for (int k{}; k < amountOfPushbacks * i; ++k)
+					vector.push_back(k);
 
-		const size_t size = vector.size();
-		const size_t capacity = vector.capacity();
-		const int front = vector.front();
-		const int back = vector.back();
-		const bool isEmpty = vector.empty();
-		vector.clear();
+				const Timepoint t2{ std::chrono::high_resolution_clock::now() };
 
-		const Timepoint t2{ std::chrono::high_resolution_clock::now() };
+				const long long time = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+				times.push_back(time);
+			}
+			times.erase(std::remove(times.begin(), times.end(), *(std::max_element(times.cbegin(), times.cend()))), times.end());
+			times.erase(std::remove(times.begin(), times.end(), *(std::min_element(times.cbegin(), times.cend()))), times.end());
 
-		const float time = std::chrono::duration<float>(t2 - t1).count();
-		timeOfSTL.push_back(time);
+			file << i << " " << "STL: " << std::fixed << std::accumulate(times.cbegin(), times.cend(), 0.f) << std::endl;
+		}
+		{
+			std::vector<long long> times{};
+			for (int j{}; j < amountOfIterations; ++j)
+			{
+				const Timepoint t1{ std::chrono::high_resolution_clock::now() };
+
+				CustomContainer<int> custom{};
+
+				for (int k{}; k < amountOfPushbacks * i; ++k)
+					custom.Push_back(k);
+
+				const Timepoint t2{ std::chrono::high_resolution_clock::now() };
+
+				const long long time = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+				times.push_back(time);
+			}
+			times.erase(std::remove(times.begin(), times.end(), *(std::max_element(times.cbegin(), times.cend()))), times.end());
+			times.erase(std::remove(times.begin(), times.end(), *(std::min_element(times.cbegin(), times.cend()))), times.end());
+
+			file << i << " " << "CUSTOM: " << std::fixed << std::accumulate(times.cbegin(), times.cend(), 0.f) << std::endl;
+		}
 	}
-	for (int i{}; i < amountOfIterations; ++i)
-	{
-		const Timepoint t1{ std::chrono::high_resolution_clock::now() };
 
-		CustomContainer<int> custom{};
-
-		for (int j{}; j < amountOfPushbacks; ++j)
-			custom.Push_back(j);
-
-		const size_t size = custom.GetSize();
-		const size_t capacity = custom.GetCapacity();
-		const int front = custom.GetFront();
-		const int back = custom.GetBack();
-		const bool isEmpty = custom.IsEmpty();
-		custom.Clear();
-
-		const Timepoint t2{ std::chrono::high_resolution_clock::now() };
-
-		const float time = std::chrono::duration<float>(t2 - t1).count();
-		timeOfCustom.push_back(time);
-	}
-
-	std::cout << "STL Average: " << std::accumulate(timeOfSTL.begin(), timeOfSTL.end(), 0.f) << std::endl;
-	std::cout << "Custom Average: " << std::accumulate(timeOfCustom.begin(), timeOfCustom.end(), 0.f) << std::endl;
+	file.close();
 
 	return 0;
 }
