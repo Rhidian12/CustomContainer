f6752d764fb8ca1b6c52b285706e8c32b0d28b91
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2022-04-13 08:22:58 +0200

    cleaning up code

--- CustomContainer/CustomContainer.h
+++ CustomContainer/CustomContainer.h
@@ -4,74 +4,19 @@
 template<typename Type>
 class CustomContainer final
 {
-	inline static constexpr size_t size = sizeof(Type);
+	inline static constexpr size_t SizeOfType = sizeof(Type);
 public:
-	CustomContainer() noexcept
-	{
-		const size_t capacity{ 5 }; // this could be any start value
-		m_pHead = static_cast<Type*>(calloc(capacity, size)); // maybe make m_pHead actually useful instead of storing memory?
-		m_pTail = m_pHead + capacity;
-	}
+	CustomContainer() = default;
 
-	~CustomContainer() noexcept
+	~CustomContainer()
 	{
 		ReleaseOldMemory(m_pHead);
 	}
 
-	CustomContainer(const CustomContainer<Type>& other) noexcept
-	{
-		const size_t capacity{ other.GetCapacity() };
-		m_pHead = static_cast<Type*>(calloc(capacity, size));
-		m_pTail = m_pHead + capacity;
-
-		for (size_t index{  }; index < other.GetSize(); ++index)
-		{
-			m_pCurrentElement = m_pHead + index + 1;
-			*m_pCurrentElement = other.At(index);
-		}
-	}
-	CustomContainer(CustomContainer<Type>&& other) noexcept
-	{
-		const size_t capacity{ other.GetCapacity() }; // this could be any start value
-		m_pHead = static_cast<Type*>(calloc(capacity, size)); // maybe make m_pHead actually useful instead of storing memory?
-		m_pTail = m_pHead + capacity;
-
-		for (size_t index{  }; index < other.GetSize(); ++index)
-		{
-			m_pCurrentElement = m_pHead + index + 1;
-			*m_pCurrentElement = other.At(index);
-		}
-
-		other.Clear();
-		other.ReleaseOldMemory(other.m_pHead);
-	}
-	CustomContainer<Type>& operator=(const CustomContainer<Type>& other) noexcept
-	{
-		const size_t capacity{ other.GetCapacity() }; // this could be any start value
-		m_pHead = static_cast<Type*>(calloc(capacity, size)); // maybe make m_pHead actually useful instead of storing memory?
-		m_pTail = m_pHead + capacity;
-
-		for (size_t index{  }; index < other.GetSize(); ++index)
-		{
-			m_pCurrentElement = m_pHead + index + 1;
-			*m_pCurrentElement = other.At(index);
-		}
-	}
-	CustomContainer<Type>& operator=(CustomContainer<Type>&& other) noexcept
-	{
-		const size_t capacity{ other.GetCapacity() }; // this could be any start value
-		m_pHead = static_cast<Type*>(calloc(capacity, size)); // maybe make m_pHead actually useful instead of storing memory?
-		m_pTail = m_pHead + capacity;
-
-		for (size_t index{  }; index < other.GetSize(); ++index)
-		{
-			m_pCurrentElement = m_pHead + index + 1;
-			*m_pCurrentElement = other.At(index);
-		}
-
-		other.Clear();
-		other.ReleaseOldMemory(other.m_pHead);
-	}
+	CustomContainer<Type>(const CustomContainer<Type>& other) noexcept;
+	CustomContainer<Type>(CustomContainer<Type>&& other) noexcept;
+	CustomContainer<Type>& operator=(const CustomContainer<Type>& other) noexcept;
+	CustomContainer<Type>& operator=(CustomContainer<Type>&& other) noexcept;
 
 	inline void Push_back(const Type& val) noexcept
 	{
@@ -173,14 +118,14 @@ public:
 
 	Type& At(const size_t index)
 	{
-		ASSERT((m_pHead + (index + 1)) > m_pCurrentElement, "Container::At() > Index was out of range!");
+		ASSERT(((m_pHead + (index + 1)) > m_pCurrentElement), "Container::At() > Index was out of range!");
 
 		return *(m_pHead + (index + 1));
 	}
 
 	const Type& At(const size_t index) const
 	{
-		ASSERT((m_pHead + (index + 1)) > m_pTail, "Container::At() > Index was out of range!");
+		ASSERT(((m_pHead + (index + 1)) > m_pTail), "Container::At() > Index was out of range!");
 
 		return *(m_pHead + (index + 1));
 	}
@@ -217,7 +162,7 @@ private:
 
 		Type* pOldHead{ m_pHead };
 
-		m_pHead = static_cast<Type*>(calloc(newCapacity, size));
+		m_pHead = static_cast<Type*>(calloc(newCapacity, SizeOfType));
 		m_pTail = m_pHead + newCapacity;
 
 		for (size_t index{ 1 }; index < oldSize; ++index)
@@ -233,3 +178,65 @@ private:
 	Type* m_pTail{ nullptr }; // same thing, only used as end of my memory block
 	Type* m_pCurrentElement{ nullptr }; // current element
 };
+
+template<typename Type>
+CustomContainer<Type>::CustomContainer(const CustomContainer<Type>& other) noexcept
+{
+	const size_t capacity{ other.GetCapacity() };
+	m_pHead = static_cast<Type*>(calloc(capacity, SizeOfType));
+	m_pTail = m_pHead + capacity;
+
+	for (size_t index{  }; index < other.GetSize(); ++index)
+	{
+		m_pCurrentElement = m_pHead + index + 1;
+		*m_pCurrentElement = other.At(index);
+	}
+}
+
+template<typename Type>
+CustomContainer<Type>::CustomContainer(CustomContainer<Type>&& other) noexcept
+{
+	const size_t capacity{ other.GetCapacity() }; // this could be any start value
+	m_pHead = static_cast<Type*>(calloc(capacity, SizeOfType)); // maybe make m_pHead actually useful instead of storing memory?
+	m_pTail = m_pHead + capacity;
+
+	for (size_t index{  }; index < other.GetSize(); ++index)
+	{
+		m_pCurrentElement = m_pHead + index + 1;
+		*m_pCurrentElement = other.At(index);
+	}
+
+	other.Clear();
+	other.ReleaseOldMemory(other.m_pHead);
+}
+
+template<typename Type>
+CustomContainer<Type>& CustomContainer<Type>::operator=(const CustomContainer<Type>& other) noexcept
+{
+	const size_t capacity{ other.GetCapacity() }; // this could be any start value
+	m_pHead = static_cast<Type*>(calloc(capacity, SizeOfType)); // maybe make m_pHead actually useful instead of storing memory?
+	m_pTail = m_pHead + capacity;
+
+	for (size_t index{  }; index < other.GetSize(); ++index)
+	{
+		m_pCurrentElement = m_pHead + index + 1;
+		*m_pCurrentElement = other.At(index);
+	}
+}
+
+template<typename Type>
+CustomContainer<Type>& CustomContainer<Type>::operator=(CustomContainer<Type>&& other) noexcept
+{
+	const size_t capacity{ other.GetCapacity() }; // this could be any start value
+	m_pHead = static_cast<Type*>(calloc(capacity, SizeOfType)); // maybe make m_pHead actually useful instead of storing memory?
+	m_pTail = m_pHead + capacity;
+
+	for (size_t index{  }; index < other.GetSize(); ++index)
+	{
+		m_pCurrentElement = m_pHead + index + 1;
+		*m_pCurrentElement = other.At(index);
+	}
+
+	other.Clear();
+	other.ReleaseOldMemory(other.m_pHead);
+}
